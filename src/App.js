bc0468eae7cd0f657296ea0b00ee6c1471b9afc4
bxc5103 <benjamin_cervantes@homedepot.com>
2019-02-20 08:53:05 -0500

    inital commit for todo app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            todo: '',
+            allTodo: []
+        }
+    }
+
+    captureTodo = (event) => {
+        this.setState({ todo: event.target.value})
+    };
+
+    addTodo = (event) => {
+        event.preventDefault()
+        const newTodo = this.state.todo;
+        const todoArray = [...this.state.allTodo, newTodo];
+        this.setState({
+            todo: '',
+            allTodo: todoArray
+        });
+        console.log(this.state.allTodo);
+    }
+
+    render() {
+        const todoList = this.state.allTodo.map((todo, index) => {
+            return <li key={index}>{todo}</li>
+        });
+        return (
+            <div className="App">
+                <h2>Todo App</h2>
+                <input placeholder='enter something to do' value={this.state.todo} onChange={this.captureTodo}/>
+                <button onClick={this.addTodo}>Add</button>
+                <ul>{todoList}</ul>
+            </div>
+        );
+    }
 }
 
 export default App;
